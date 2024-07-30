20c2023a22cd8d3368bacfa3b453fba749634622
Nicoov <nicolas_ignacio_1@hotmail.com>
2024-07-29 21:38:19 -0400

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-f72663e5-5d80-4d16-a6c6-fed399f4bfb3.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
